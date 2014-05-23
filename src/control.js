00a9602758468c2ba04d2e0be4998c2cc5b6e4ec
hakatashi <hakatasiloving@gmail.com>
2014-05-24 01:33:31 +0900

    First step

--- src/control.js
+++ src/control.js
@@ -178,7 +178,7 @@ function getParameterByName(name) {
 	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
 	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
 		results = regex.exec(location.search);
-	return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+	return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
 // from http://stackoverflow.com/questions/6466135/
