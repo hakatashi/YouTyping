5715fc650fa3b57a4eff20c93383aba0f14e93b2
hakatashi <hakatasiloving@gmail.com>
2014-09-02 16:26:56 +0900

    Remove -webkit prefix

--- src/screen.js
+++ src/screen.js
@@ -604,7 +604,7 @@ var Screen = function (element, settings) {
 			height: this.settings.height + 'px',
 			margin: '0 auto',
 			position: 'relative',
-			'-webkit-perspective': this.settings['3d'] ? '1000px' : '0px'
+			'perspective': this.settings['3d'] ? '1000px' : '0px'
 		})[0],
 
 		player: $('<div/>', {
@@ -614,9 +614,9 @@ var Screen = function (element, settings) {
 			height: this.settings.height + 'px',
 			display: 'block',
 			'z-index': 0,
-			'-webkit-transform': this.settings['3d']
-			                     ? 'rotateY(-30deg) scale(0.8) translateX(-100px)'
-			                     : ''
+			'transform': this.settings['3d']
+			             ? 'rotateY(-30deg) scale(0.8) translateX(-100px)'
+			             : ''
 		})[0],
 
 		screen: $('<canvas/>', {
